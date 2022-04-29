4f5dbb410f27be8ce05f62912c845b534bb56e9e
kndt <mnxx@protonmail.ch>
2022-04-29 15:12:44 +0100

    init

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,55 +1,42 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  PoolDeployer,
-  AdminChanged,
-  BeaconUpgraded,
-  Upgraded
-} from "../generated/PoolDeployer/PoolDeployer"
-import { ExampleEntity } from "../generated/schema"
+import { log, BigInt, Bytes } from '@graphprotocol/graph-ts'
 
-export function handleAdminChanged(event: AdminChanged): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+import { DeployPoolCall, DeployNewNftPool, PoolDeployer } from "../generated/PoolDeployer/PoolDeployer"
+import { PoolDeployerEntity, NftPoolEntity } from "../generated/schema"
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+// export function handleDeployPool(call: DeployPoolCall): void {
+//   let id = call.transaction.hash.toHex();
+//   let poolDeployer = PoolDeployerEntity.load(id);
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+//   log.info('handleDeployPool call.transaction.hash id {}', [id.toString()]);
+
+//   log.info('handleDeployPool call.inputs.operator id {}', [call.inputs.operator.toHex().toString()]);
+
+//   if (!poolDeployer) {
+//     poolDeployer = PoolDeployerEntity.load(call.inputs.operator.toHex());
+//   }
+
+//   if (!poolDeployer) {
+//     poolDeployer = new PoolDeployerEntity(call.inputs.operator.toHex());
+//   }
+
+//   poolDeployer.save();
+// }
+
+export function handleDeployNewNftPool(event: DeployNewNftPool): void {
+  let contract = PoolDeployer.bind(event.address)
+
+  let id = event.transaction.hash.toHex();
+  let poolDeployer = PoolDeployerEntity.load(id);
+
+  if (!poolDeployer) {
+    poolDeployer = new PoolDeployerEntity(id);
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  poolDeployer.poolCount = contract.poolCount();
 
-  // Entity fields can be set based on event parameters
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
+  const count = contract.poolCount().toU32();
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  (poolDeployer.allPools as Array<Bytes>).push(contract.allPools(BigInt.fromI32(count)));
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.admin(...)
-  // - contract.implementation(...)
+  poolDeployer.save();
 }
-
-export function handleBeaconUpgraded(event: BeaconUpgraded): void {}
-
-export function handleUpgraded(event: Upgraded): void {}
